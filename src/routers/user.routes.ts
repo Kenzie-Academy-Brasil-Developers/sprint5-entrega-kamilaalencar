186406cb08ed0f8e50854920c7056fc2a0ea9855
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-05-15 22:41:02 -0300

    feat:list users criation

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import userCreateController from "../controllers/users/userCreate.controller";
+import userListController from "../controllers/users/userList.controller";
 
 const routes = Router();
 
 routes.post("/users", userCreateController);
-routes.get("/users");
+routes.get("/users", userListController);
 routes.get("/users/:id");
 routes.patch("/users/:id");
 routes.delete("/users/:id");
